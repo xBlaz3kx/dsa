cdc5b9c264a2e168802a508149c230ea64d1592f
bdular <blaz.dular@gmail.com>
2025-03-17 23:10:03 +0100

    feat: added stack test for size, empty and peek

--- structures/stack/stack_test.go
+++ structures/stack/stack_test.go
@@ -13,17 +13,25 @@ func TestStack(t *testing.T) {
 	stack.Push(3)
 	stack.Push(4)
 
+	assert.Equal(t, 4, stack.Size())
+
 	val, err := stack.Pop()
 	assert.NoError(t, err)
 	assert.Equal(t, 4, *val)
+
+	assert.Equal(t, 3, stack.Size())
 
 	val, err = stack.Pop()
 	assert.NoError(t, err)
 	assert.Equal(t, 3, *val)
 
+	assert.Equal(t, 2, stack.Size())
+
 	val, err = stack.Pop()
 	assert.NoError(t, err)
 	assert.Equal(t, 2, *val)
+
+	assert.Equal(t, 1, stack.Size())
 
 	val, err = stack.Pop()
 	assert.NoError(t, err)
@@ -31,4 +39,31 @@ func TestStack(t *testing.T) {
 
 	val, err = stack.Pop()
 	assert.ErrorContains(t, err, "stack is empty")
+
+	assert.Equal(t, 0, stack.Size())
+	assert.True(t, stack.IsEmpty())
+}
+
+func TestStackPeek(t *testing.T) {
+	stack := NewStack[int]()
+	stack.Push(1)
+	stack.Push(2)
+	stack.Push(3)
+	stack.Push(4)
+
+	val, err := stack.Peek()
+	assert.NoError(t, err)
+	assert.Equal(t, 4, *val)
+
+	val, err = stack.Peek()
+	assert.NoError(t, err)
+	assert.Equal(t, 4, *val)
+
+	val, err = stack.Pop()
+	assert.NoError(t, err)
+	assert.Equal(t, 4, *val)
+
+	val, err = stack.Peek()
+	assert.NoError(t, err)
+	assert.Equal(t, 3, *val)
 }
